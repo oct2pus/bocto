8bc251137669899ceb19ad5f68c5ee7b2532375d
Octopus Octopus <oct2pus@jade.moe>
2021-05-23 01:32:40 -0500

    add response type, add comments to command.go

--- command.go
+++ command.go
@@ -2,7 +2,14 @@ package bocto
 
 import "github.com/bwmarrin/discordgo"
 
+// command is a wrapper for a Function, and it specifies if its requires a prefix to used.
+// this is depreciated as of 5/23/2021, as discord has added slash commands.
+// will be supported for at least ? months while existing servers add the slash
+// command privlege.
 type command struct {
 	Function func(Bot, *discordgo.MessageCreate, []string)
 	Prefix   bool
 }
+
+// response is a message thats activated by a message without a prefix.
+type response func(Bot, *discordgo.MessageCreate, []string)
